0db410f5b939558a715b17fa329c2713d20b7bd6
joncrall <jon.crall@kitware.com>
2022-07-09 22:50:12 -0400

    Run more tests

--- run_tests.py
+++ run_tests.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
             '--cov-report', 'term',
             '--cov-report', 'xml',
             '--cov=' + package_name,
-            modpath,
+            modpath, '.'
         ]
         if is_cibuildwheel():
             pytest_args.append('--cov-append')
